4bde875f34ca89141305b4b437c84eaba00f33b8
frckbrice <maebrie2017@gmail.com>
2023-04-09 10:23:22 +0100

    adding constraint

--- knapsack.js
+++ knapsack.js
@@ -5,12 +5,20 @@ function Objects(name, weight, value) {
   this.value = value;
 }
 
+//  the knapSack constructor
+function bags(capacity, items, weight, value) {
+  this.capacity = capacity;
+  this.items = items;
+  this.weight = weight;
+  this.value = value;
+}
+
 /*
  function to get a particular random list of objects given an interval of elements 
  to avoid entring objects manually*/
 
 const items = (min, max) => {
-  // let's make items array to not be empty and the computation start at index 1 
+  // let's make items array to not be empty and the computation start at index 1
   let arr = [{}];
   for (let i = min; i <= max; i++) {
     let n1 = Math.floor(Math.random() * (max - min)) + min;
@@ -23,14 +31,14 @@ const items = (min, max) => {
 
 // the function to get the maximum value between a set of objects with weight and value
 
-function tableOfMaxProfit(capacity, items) {
+function knapSack(capacity, items) {
   // some check first
   if (!(capacity > 0 && items.length)) {
     console.warn(
-      "capacity should be a positive (> 0) number and array of object not empty"
+      "capacity must be a positive (> 0) number and array of object(s) not empty"
     );
   } else {
-    let itemsSize = items.length; 
+    let itemsSize = items.length;
     let tabMaxProfit = [];
 
     //create and initialize table of maximum profit
@@ -51,7 +59,8 @@ function tableOfMaxProfit(capacity, items) {
     let i = 0,
       w = 0,
       optimalValue = 0,
-      indexOptimalValue = 0;
+      indexOptimalValue = 0,
+      weigh = 0;
     //print list of object used for this case of searching the optimal value
     console.log();
     console.log("The list of objects used for computation of optimal value: ");
@@ -60,6 +69,7 @@ function tableOfMaxProfit(capacity, items) {
     //fill the bags from o kg weight to the maximum capacity weight
 
     for (i = 1; i < newItems.length; i++) {
+
       // get the weight and the value of the current item
       let wi = newItems[i].weight;
       let vi = newItems[i].value;
@@ -79,35 +89,53 @@ function tableOfMaxProfit(capacity, items) {
         indexOptimalValue = i;
       }
     }
+
     // return tabMaxProfit;
     console.log();
     console.log("The table after computing the optimal value is: ");
     console.table(tabMaxProfit);
-    // }
+
     // let optimalValue = tabMaxProfit[newItems.length - 1][capacity];
     console.log(
       "the optimal value for this sequence of objects is : " + optimalValue
     );
+
     // serching the items that led us to optimal value
     // let nLines = newItems.length;
     let c = capacity;
     let objectUsed = [];
+
     //going back in the table from the optimal value index to the original value index
     for (let i = indexOptimalValue; i >= 1; i--) {
-      if (tabMaxProfit[i][c] !== tabMaxProfit[i - 1][c] && newItems[i].value) {
+      if (tabMaxProfit[i][c] > tabMaxProfit[i - 1][c]) {
         objectUsed.push(i);
         c -= newItems[i].weight;
-        //to show the object used to achieve the goal while taking them
-        //  console.log(objectUsed);
+        weigh += newItems[i].weight;
       }
     }
-
     console.log();
     console.log(
       "the list of elements chosen to rich the optimal value in order is: "
     );
     objectUsed = objectUsed.reverse();
-    console.log(objectUsed);
+
+    /* to show the object used to achieve the goal while taking them
+          console.log(objectUsed); */
+    // console.log(objectUsed);
+    console.log();
+    let newBags = new bags(
+      capacity + "kg",
+      objectUsed,
+      weigh + "kg",
+      optimalValue + "$"
+    );
+    console.log();
+    console.log(
+      "the return object containing net capacity of the bags, set of used items, total weight of such items and the optimal value is :  "
+    );
+    console.log();
+    console.log(newBags);
   }
 }
-console.log(tableOfMaxProfit(-12, items(10,20)));
+
+console.log(knapSack(15, items(1, 20)));
