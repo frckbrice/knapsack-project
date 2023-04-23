b33078ea9c2530cf5d5a17ba2c4f9b0f2ea9c535
frckbrice <maebrie2017@gmail.com>
2023-04-23 17:21:34 +0100

    midterm of project

--- knapsack.js
+++ knapsack.js
@@ -1,3 +1,78 @@
+//* to bind html elements
+
+const addObjectButton = document.querySelector(".add-object-btn");
+const doneButton = document.querySelector(".done-btn");
+const ulListOfObjects = document.querySelector(".ul-of-objects");
+
+//* add events
+// doneButton.addEventListener("click", displayResults);
+addObjectButton.addEventListener("click", addOjects);
+addObjectButton.addEventListener("click", createArrayObject);
+doneButton.addEventListener("click", knapSack);
+
+function addOjects(e) {
+  e.preventDefault();
+  const inputName = document.querySelector("#input-name").value;
+  const inputWeight = parseFloat(document.querySelector("#input-weight").value);
+  const inputValue = parseFloat(document.querySelector("#input-value").value);
+
+  const line = document.createElement("li");
+  line.className = "li-for-objects";
+
+  const pTag1 = document.createElement("p");
+  pTag1.appendChild(document.createTextNode(inputName));
+  pTag1.classList.add("ptag");
+
+  const pTag2 = document.createElement("p");
+  pTag2.appendChild(document.createTextNode(inputWeight));
+  pTag2.classList.add("ptag");
+
+  const pTag3 = document.createElement("p");
+  pTag3.classList.add("ptag");
+  pTag3.appendChild(document.createTextNode(inputValue));
+
+  line.appendChild(pTag1);
+  line.appendChild(pTag2);
+  line.appendChild(pTag3);
+
+  ulListOfObjects.appendChild(line);
+
+  //   document.querySelector("#input-name").value = "";
+  //   document.querySelector("#input-weight").value = "";
+  //  document.querySelector("#input-value").value = "";
+}
+
+function createArrayObject() {
+let inputName = document.querySelector("#input-name").value;
+let inputWeight = parseFloat(document.querySelector("#input-weight").value);
+let inputValue = parseFloat(document.querySelector("#input-value").value);
+
+let arrayOfObject = [{}];
+console.log(inputValue);
+console.log(inputWeight);
+//to not allow empty input
+if (!inputName || !inputValue || !inputWeight) {
+  alert("empty input value not allowed");
+  return false;
+} else {
+  // to get inputs while the number is less than 20
+  if (ulListOfObjects.childElementCount <= 20) {
+    // to create an array of abjects
+Array.from(ulListOfObjects.children).forEach((line) => {
+  console.log(line.children[2].textContent);
+  inputName = parse(line.children[0].textContent);
+  inputWeight = line.children[1].textContent;
+  inputValue = line.children[2].textContent;
+
+  let item = new Objects(`${inputName}`, `${inputWeight}`, `${inputValue}`);
+
+  arrayOfObject.push(item);
+});
+  }
+  console.log(arrayOfObject);
+  return arrayOfObject;
+}
+}
 
 //* create the constructor of objects that we need for the purpose
 function Objects(name, weight, value) {
@@ -17,30 +92,41 @@ function bags(capacity, items, weight, value) {
 }
 
 /*
-* function to get a particular random list of objects given an interval of elements 
-* to avoid entring objects manually*/
+ * function to get a particular random list of objects given an interval of elements
+ * to avoid entring objects manually*/
 
-const items = (min, max) => {
-  //* let's make items array to not be empty and the computation start at index 1
-  let arr = [{}];
-  for (let i = min; i <= max; i++) {
-    let n1 = Math.floor(Math.random() * (max - min)) + min;
-    let n2 = Math.floor(Math.random() * (max - min)) + min;
-    let item = new Objects(`${i}`, n1, n2);
-    arr.push(item);
-  }
-  return arr;
-};
+// const items = (min, max) => {
+//   //* let's make items array to not be empty and the computation start at index 1
+//   let arr = [{}];
+//   for (let i = min; i <= max; i++) {
+//     let n1 = Math.floor(Math.random() * (max - min)) + min;
+//     let n2 = Math.floor(Math.random() * (max - min)) + min;
+//     let item = new Objects(`${i}`, n1, n2);
+//     arr.push(item);
+//   }
+//   return arr;
+// };
 
 //* the function to get the maximum value within a set of objects with weight and value
 
-function knapSack(capacity, items) {
+function knapSack(e) {
+  e.preventDefault();
+  const capacity = parseFloat(
+    document.querySelector(".input-for-capacity").value
+  );
+
+  const items = createArrayObject();
+
   // some check first
   if (!(capacity > 0 && items.length)) {
-    console.warn(
-      "capacity must be a positive (> 0) number and array of object(s) not empty"
+    alert(
+      "capacity must be a positive (> 0) number and list of object(s) not empty"
     );
+    return false;
   } else {
+    console.log("the receive array is : ");
+    console.table(items);
+
     let itemsSize = items.length;
     let tabMaxProfit = [];
 
@@ -57,8 +143,8 @@ function knapSack(capacity, items) {
     console.log("the initialize table is : ");
     console.table(tabMaxProfit);
     // to get a particular list of objects between an interval given
-    let newItems = [];
-    newItems = items;
+    let newitems = [];
+    newitems = items;
     let i = 0,
       w = 0,
       optimalValue = 0,
@@ -67,14 +153,14 @@ function knapSack(capacity, items) {
     //print list of object used for this case of searching the optimal value
     console.log();
     console.log("The list of objects used for computation of optimal value: ");
-    console.log(newItems);
+    console.log(newitems);
     console.log();
     //fill the bags from o kg weight to the maximum capacity weight
 
-    for (i = 1; i < newItems.length; i++) {
+    for (i = 1; i < newitems.length; i++) {
       // get the weight and the value of the current item
-      let wi = newItems[i].weight;
-      let vi = newItems[i].value;
+      let wi = newitems[i].weight;
+      let vi = newitems[i].value;
       for (w = 1; w <= capacity; w++) {
         // to have the right value in the table
         if (w >= wi) {
@@ -107,8 +193,8 @@ function knapSack(capacity, items) {
     for (let i = indexOptimalValue; i >= 1; i--) {
       if (tabMaxProfit[i][c] > tabMaxProfit[i - 1][c]) {
         objectUsed.push(i);
-        c -= newItems[i].weight;
-        weigh += newItems[i].weight;
+        c -= newitems[i].weight;
+        weigh += newitems[i].weight;
       }
     }
     console.log();
@@ -131,64 +217,12 @@ function knapSack(capacity, items) {
     );
     console.log();
     console.log(newBags);
+    document.querySelector(".resulting-bag").innerHTML = JSON.stringify(
+      newBags,
+      null,
+      2
+    );
   }
 }
 
 // console.log(knapSack(15, items(1, 20)));
-
-//* to bind elements from html
-const inputCapacityValue = parseInt(document.querySelector(".input-for-capacity").value);
-const addObjectButton = document.querySelector(".add-object-btn");
-const doneButton = document.querySelector(".done-btn");
-const ulOfObjects = document.querySelector(".ul-of-objects");
-
-//* add events
-// doneButton.addEventListener("click", displayResults);
-addObjectButton.addEventListener("click", addOjects);
-
-function addOjects() {
-  const inputName = document.querySelector(".input-name").value;
-  const inputValue = parseInt(document.querySelector(".input-value").value);
-  const inputWeight = parseInt(document.querySelector(".input-weight").value);
-
-  const line = document.createElement("li");
-  line.className = "li-for-objects";
-
-  const pTag1 = document.createElement("p");
-  pTag1.appendChild(document.createTextNode(inputName));
-
-  const pTag2 = document.createElement("p");
-  pTag2.appendChild(document.createTextNode(inputWeight));
-
-  const pTag3 = document.createElement("p");
-  pTag3.appendChild(document.createTextNode(inputValue));
-
-  line.appendChild(pTag1);
-  line.appendChild(pTag2);
-  line.appendChild(pTag3);
-
-  ulOfObjects.appendChild(line);
-  document.querySelector(".input-name").value = '';
-  document.querySelector(".input-value").value = '';
-  document.querySelector(".input-weight").value = '';
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
