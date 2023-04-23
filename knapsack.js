63d0bcaffc50ad95cf144010e7cab9a6fb56e608
frckbrice <maebrie2017@gmail.com>
2023-04-23 07:47:53 +0100

    adding objects to list

--- knapsack.js
+++ knapsack.js
@@ -1,24 +1,27 @@
-// create the constructor of objects that we need for the purpose
+
+//* create the constructor of objects that we need for the purpose
 function Objects(name, weight, value) {
   this.name = name;
   this.weight = weight;
   this.value = value;
+  return this;
 }
 
-//  the knapSack constructor
+//*  the knapSack constructor
 function bags(capacity, items, weight, value) {
   this.capacity = capacity;
   this.items = items;
   this.weight = weight;
   this.value = value;
+  return this;
 }
 
 /*
- function to get a particular random list of objects given an interval of elements 
- to avoid entring objects manually*/
+* function to get a particular random list of objects given an interval of elements 
+* to avoid entring objects manually*/
 
 const items = (min, max) => {
-  // let's make items array to not be empty and the computation start at index 1
+  //* let's make items array to not be empty and the computation start at index 1
   let arr = [{}];
   for (let i = min; i <= max; i++) {
     let n1 = Math.floor(Math.random() * (max - min)) + min;
@@ -29,7 +32,7 @@ const items = (min, max) => {
   return arr;
 };
 
-// the function to get the maximum value between a set of objects with weight and value
+//* the function to get the maximum value within a set of objects with weight and value
 
 function knapSack(capacity, items) {
   // some check first
@@ -69,7 +72,6 @@ function knapSack(capacity, items) {
     //fill the bags from o kg weight to the maximum capacity weight
 
     for (i = 1; i < newItems.length; i++) {
-
       // get the weight and the value of the current item
       let wi = newItems[i].weight;
       let vi = newItems[i].value;
@@ -82,30 +84,26 @@ function knapSack(capacity, items) {
           );
         }
       }
-      /* to get the optimal value of the table and its index. 
-    sometimes the optimal value is not right-bottom corner element */
+      //* *to get the optimal value of the table and its index.     sometimes the optimal value is not right-bottom corner element
       if (tabMaxProfit[i][capacity] >= optimalValue) {
         optimalValue = tabMaxProfit[i][capacity];
         indexOptimalValue = i;
       }
     }
 
-    // return tabMaxProfit;
+    //* return tabMaxProfit;
     console.log();
     console.log("The table after computing the optimal value is: ");
     console.table(tabMaxProfit);
-
-    // let optimalValue = tabMaxProfit[newItems.length - 1][capacity];
     console.log(
       "the optimal value for this sequence of objects is : " + optimalValue
     );
 
-    // serching the items that led us to optimal value
-    // let nLines = newItems.length;
+    //* serching the items that led us to optimal value
     let c = capacity;
     let objectUsed = [];
 
-    //going back in the table from the optimal value index to the original value index
+    //*going back in the table from the optimal value index to the original value index to get elements of the used list
     for (let i = indexOptimalValue; i >= 1; i--) {
       if (tabMaxProfit[i][c] > tabMaxProfit[i - 1][c]) {
         objectUsed.push(i);
@@ -119,9 +117,7 @@ function knapSack(capacity, items) {
     );
     objectUsed = objectUsed.reverse();
 
-    /* to show the object used to achieve the goal while taking them
-          console.log(objectUsed); */
-    // console.log(objectUsed);
+    //* to show the object used to achieve the goal while taking them
     console.log();
     let newBags = new bags(
       capacity + "kg",
@@ -138,4 +134,61 @@ function knapSack(capacity, items) {
   }
 }
 
-console.log(knapSack(15, items(1, 20)));
+// console.log(knapSack(15, items(1, 20)));
+
+//* to bind elements from html
+const inputCapacityValue = parseInt(document.querySelector(".input-for-capacity").value);
+const addObjectButton = document.querySelector(".add-object-btn");
+const doneButton = document.querySelector(".done-btn");
+const ulOfObjects = document.querySelector(".ul-of-objects");
+
+//* add events
+// doneButton.addEventListener("click", displayResults);
+addObjectButton.addEventListener("click", addOjects);
+
+function addOjects() {
+  const inputName = document.querySelector(".input-name").value;
+  const inputValue = parseInt(document.querySelector(".input-value").value);
+  const inputWeight = parseInt(document.querySelector(".input-weight").value);
+
+  const line = document.createElement("li");
+  line.className = "li-for-objects";
+
+  const pTag1 = document.createElement("p");
+  pTag1.appendChild(document.createTextNode(inputName));
+
+  const pTag2 = document.createElement("p");
+  pTag2.appendChild(document.createTextNode(inputWeight));
+
+  const pTag3 = document.createElement("p");
+  pTag3.appendChild(document.createTextNode(inputValue));
+
+  line.appendChild(pTag1);
+  line.appendChild(pTag2);
+  line.appendChild(pTag3);
+
+  ulOfObjects.appendChild(line);
+  document.querySelector(".input-name").value = '';
+  document.querySelector(".input-value").value = '';
+  document.querySelector(".input-weight").value = '';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
